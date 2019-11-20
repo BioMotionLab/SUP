053017b6f14d4f79d845c6c9c364effc0b07b840
AdamBebko <adambebko@gmail.com>
2019-11-20 11:04:52 -0500

    made variables for JSON keys

--- JL_displayMoSh/Assets/Scripts/MoShAnimation/MoShAnimationJSON.cs
+++ JL_displayMoSh/Assets/Scripts/MoShAnimation/MoShAnimationJSON.cs
@@ -12,6 +12,13 @@ using System;
 /// </summary>
 public class MoShAnimationJSON : MoShAnimation {
 
+    const string GenderKey = "gender";
+    const string MaleString   = "male";
+    const string FemaleString = "female";
+    const string FPSKey = "fps";
+    const string TransKey = "trans";
+    const string BetasKey = "betas";
+    const string PosesKey = "poses";
     /// <summary>
     /// 
     /// </summary>
@@ -40,33 +47,34 @@ public class MoShAnimationJSON : MoShAnimation {
 	public void LoadAnimationJSON(JSONNode moshjson)
 	{
 		// set gender. 
-		if (moshjson["gender"] == "male") {
+        JSONNode genderNode = moshjson[GenderKey];
+        if (genderNode.IsNull) throw new Exception("File does not contain a gender field.");
+        
+        if (genderNode == MaleString) {
 			gender = Genders.MALE;
             Jc = JointCalculator.Male;
-		} else if (moshjson["gender"] == "female") {
-			gender = Genders.FEMALE;
-            Jc = JointCalculator.Female;
 		} else {
-            if (moshjson["gender"].IsNull)
-            {
-                throw new Exception("File does not contain a gender field.");
+            if (genderNode == FemaleString) {
+                gender = Genders.FEMALE;
+                Jc = JointCalculator.Female;
+            } else {
+                throw new Exception ("Unexpected value for gender in JSON file.");
             }
-            throw new Exception ("Unexpected value for gender in JSON file.");
-		}
-
-        if (moshjson["fps"].IsNull) {
-            throw new Exception("JSON has no fps field.");
         }
-
-        fps = moshjson["fps"];
+        
+        JSONNode fpsNode = moshjson[FPSKey];
+        if (fpsNode.IsNull) throw new Exception("JSON has no fps field.");
+        fps = fpsNode;
         SourceFPS = fps;
-        length = moshjson["trans"].Count;
+
+        
+        length = moshjson[TransKey].Count;
         SourceLength = length;
         duration = length / (float)fps;
 
-        for (int i = 0; i < 10; i++)
-        {
-            betas[i] = moshjson["betas"][i];
+        for (int i = 0; i < 10; i++) {
+            
+            betas[i] = moshjson[BetasKey][i];
         }
                 
         Translation = new Vector3[length];
@@ -84,9 +92,9 @@ public class MoShAnimationJSON : MoShAnimation {
             // I'm pretty sure maya is right handed z-up. 
             // Unity is right handed y up? 
             float x, y, z;
-            x = moshjson["trans"][i][0];
-            y = moshjson["trans"][i][1];
-            z = moshjson["trans"][i][2];
+            x = moshjson[TransKey][i][0];
+            y = moshjson[TransKey][i][1];
+            z = moshjson[TransKey][i][2];
             if (ZAxisUp) {
                 x = -x;
             }
@@ -100,10 +108,10 @@ public class MoShAnimationJSON : MoShAnimation {
                 // Quaternion components must also be flipped. But the original didn't check what the up axis is. 
                 float qx, qy, qz, qw;
                 // Arrrggg the error was that it was getting cast to an integer or something because I was multiplying by -1, not -1f.
-                qx = -1.0f * moshjson["poses"][i][j][0];
-                qy = moshjson["poses"][i][j][1];
-                qz = moshjson["poses"][i][j][2];
-                qw = -1.0f * moshjson["poses"][i][j][3];
+                qx = -1.0f * moshjson[PosesKey][i][j][0];
+                qy = moshjson[PosesKey][i][j][1];
+                qz = moshjson[PosesKey][i][j][2];
+                qw = -1.0f * moshjson[PosesKey][i][j][3];
                 //Debug.Log("frame " + i + ": qx = " + qx + " qy = " + qy + "qz = " + qz + " qw = " + qw);
                 Poses[i, j] = new Quaternion(qx, qy, qz, qw);
 
