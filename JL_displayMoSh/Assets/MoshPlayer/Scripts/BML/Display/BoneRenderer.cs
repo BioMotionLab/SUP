6b5f27a02ac1e1300222d54c7c6f2d6e7f6efd75
AdamBebko <adambebko@gmail.com>
2019-12-03 17:57:34 -0500

    minor code moving and method refactors

--- JL_displayMoSh/Assets/MoshPlayer/Scripts/BML/Display/BoneRenderer.cs
+++ JL_displayMoSh/Assets/MoshPlayer/Scripts/BML/Display/BoneRenderer.cs
@@ -12,13 +12,16 @@ namespace MoshPlayer.Scripts.BML.Display {
         SMPLSettings settings;
     
         void Awake() {
-            lineRenderer = GetComponent<LineRenderer>(); //starts disabled to avoid artifacts the first frame.
+            lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.enabled = false;
         }
 
         public void Init(Transform boneParent, Transform boneChild, SMPLSettings settingsFile) {
             parent = boneParent;
             child = boneChild;
             settings = settingsFile;
+            
+            SetDifferentSideColorsIfNeeded();
         }
 
         void Update() {
@@ -26,21 +29,27 @@ namespace MoshPlayer.Scripts.BML.Display {
             lineRenderer.SetPositions(positions);
             lineRenderer.startWidth = settings.DisplaySettings.BoneWidth;
             lineRenderer.endWidth = settings.DisplaySettings.BoneWidth;
+            
+            EnsureLineRendererEnabled();
+        }
 
+        /// <summary>
+        /// Starts disabled to avoid artifacts the first frame when it's huge and not initialized. Need to enable it when ready to display.
+        /// </summary>
+        void EnsureLineRendererEnabled() {
+            lineRenderer.enabled = true;
+        }
+
+        void SetDifferentSideColorsIfNeeded() {
             if (!settings.DisplaySettings.DrawSidesDifferentColors) return;
-            
-            
+
             if (child.name.Contains(SMPLConstants.LeftBonePrefix)) {
                 lineRenderer.material = settings.DisplaySettings.LeftSideMaterial;
             }
+
             if (child.name.Contains(SMPLConstants.RightBonePrefix)) {
                 lineRenderer.material = settings.DisplaySettings.RightSideMaterial;
             }
-
-            if (!lineRenderer.enabled) {
-                lineRenderer.enabled = true;
-            }
         }
-    
     }
 }
