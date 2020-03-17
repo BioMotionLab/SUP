6e9b39c511245f4587a0b14dd85c0865aea9f6d6
Adam Bebko <adambebko@gmail.com>
2020-03-17 16:16:43 -0400

    Improved review pane error reporting

--- UnityMoshViewer/Assets/MoshPlayer/AnimationReviewer/SaveNoteButton.cs
+++ UnityMoshViewer/Assets/MoshPlayer/AnimationReviewer/SaveNoteButton.cs
@@ -1,7 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace MoshPlayer.AnimationReviewer {
     public class SaveNoteButton : MonoBehaviour {
@@ -11,26 +15,72 @@ namespace MoshPlayer.AnimationReviewer {
 
         [SerializeField]
         TMP_InputField noteText = default;
-    
-    
+
+        [SerializeField] float noteClearTime = 1.5f;
+        
+        List<NoteChangedFlag> noteChangedFlags = new List<NoteChangedFlag>();
+        
+        void OnEnable() {
+            noteText.onValueChanged.AddListener(NoteChanged);
+        }
+
+        void OnDisable() {
+            noteText.onValueChanged.RemoveListener(NoteChanged);
+        }
+        
+
         [PublicAPI]
         public void SaveNoteToFile() {
             string note = noteText.text;
             string newLine = reviewPanel.CurrentAnims + "," + note + "\n";
-        
-            Debug.Log(note);
-            if (string.IsNullOrWhiteSpace(note)) return;
-        
-            if (string.IsNullOrWhiteSpace(reviewPanel.CurrentAnims)) Debug.LogWarning("Review pane could not gather current anims");
-        
-            if (File.Exists(reviewPanel.ReviewFilePath)) {
-            
-                File.AppendAllText(reviewPanel.ReviewFilePath, newLine);
-            }
-            else {
-                noteText.text = "NO REVIEW FILE FOUND";
+
+            if (!File.Exists(reviewPanel.ReviewFilePath)) {
+                StartCoroutine(DisplayError("NO REVIEW FILE FOUND"));
+                return;
             }
 
+            
+            if (string.IsNullOrWhiteSpace(note)) { ;
+                StartCoroutine(DisplayError("NO NOTE!"));
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(reviewPanel.CurrentAnims)) {
+                Debug.LogWarning("Review pane could not gather current anims");
+                StartCoroutine(DisplayError("NO CURRENT ANIMATIONS"));
+                return;
+            }
+            Debug.Log($"Saving note: {note}");
+            File.AppendAllText(reviewPanel.ReviewFilePath, newLine);
+            noteText.SetTextWithoutNotify("");
+            StartCoroutine(DisplayError("SAVED."));
+        }
+
+        IEnumerator DisplayError(string errorText) {
+            NoteChangedFlag changedFlag = new NoteChangedFlag();
+            noteChangedFlags.Add(changedFlag);
+            string currentText = noteText.text;
+            float startTime = Time.time;
+            noteText.SetTextWithoutNotify(errorText);
+            while (Time.time - startTime < noteClearTime) {
+                yield return null;
+            }
+            if (!changedFlag.Changed) noteText.SetTextWithoutNotify(currentText);
+            noteChangedFlags.Remove(changedFlag);
+        }
+
+        void NoteChanged(string unused) {
+            foreach (NoteChangedFlag noteChangedFlag in noteChangedFlags) {
+                noteChangedFlag.Flip();
+            }
+        }
+
+        class NoteChangedFlag {
+           
+            public bool Changed = false;
+            public void Flip() {
+                Changed = true;
+            }
         }
     }
 }
--- UnityMoshViewer/Assets/MoshPlayer/Scripts/InGameUI/RenderSelector.cs
+++ UnityMoshViewer/Assets/MoshPlayer/Scripts/InGameUI/RenderSelector.cs
@@ -1,9 +1,16 @@
-﻿using MoshPlayer.Scripts.Playback;
+﻿using System;
+using MoshPlayer.Scripts.Playback;
 using UnityEngine;
 
 namespace MoshPlayer.Scripts.InGameUI {
-    public class RenderSelector : MonoBehaviour
-    {
+    public class RenderSelector : MonoBehaviour {
+
+        [SerializeField] GameObject manualPoseManipulationPanel;
+
+        void Awake() {
+            if (!Application.isEditor) manualPoseManipulationPanel.SetActive(false);
+        }
+
         public void ChangeLiveBodyShape(bool changeShape) {
             PlaybackEventSystem.ChangeLiveBodyShapeRendering(changeShape);
         }
