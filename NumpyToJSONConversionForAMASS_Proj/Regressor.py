d36586b7e5fae38e1a588427ba2c6b654fe7f60b
Adam Bebko <adambebko@gmail.com>
2020-04-06 18:44:27 -0400

    created build tool

--- NumpyToJSONConversionForAMASS_Proj/Regressor.py
+++ NumpyToJSONConversionForAMASS_Proj/Regressor.py
@@ -2,6 +2,7 @@ import numpy as np
 import json
 import sys
 
+
 # This class converts a model.npz into a Unity-readable Json file.
 # See the AMASSConverterExamples file to see how it is used.
 class SMPLHRegressorToJSON:
